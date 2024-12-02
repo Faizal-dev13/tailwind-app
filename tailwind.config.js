e4eae731220cc0b1dc252cc750735ba652a66d56
Faizal-dev13 <ahmadfaizal1320@gmail.com>
2024-12-02 08:52:47 +0700

    add tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [], // Kosongkan content untuk menghindari filter
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
